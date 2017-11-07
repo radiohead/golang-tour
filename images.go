9c7449f577f6d7f92f66e8ac849d08f30bf0e8f9
Igor Suleymanov <radiohead@users.noreply.github.com>
2017-11-07 21:44:56 +0200

    Create images.go

---
+++ images.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
+
+type Image struct{
+	w,h int
+	clrModel color.Model
+}
+
+func (img Image) At(x, y int) color.Color {
+	w1,h1 := img.w+1, img.h+1
+	x1,y1 := x+1, y+1
+
+	return color.RGBA{
+		R: uint8((img.w*img.h + x*y) % 255),
+		G: uint8((img.w*img.h - x*y) % 255),
+		B: uint8((w1/h1 * x1/y1) % 255),
+		A: uint8((w1*h1 / x1*y1) % 255),
+	}
+}
+
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, img.w, img.h)
+}
+
+func (img Image) ColorModel() color.Model {
+	return img.clrModel
+}
+
+func main() {
+	m := Image{
+		w: 300,
+		h: 300,
+		clrModel: color.RGBAModel,
+	}
+
+	pic.ShowImage(m)
+}
